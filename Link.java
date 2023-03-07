ae5e3413e84a32e49ed3a966b7f7b140dc52f887
Cade DuPont <cadedupont45@gmail.com>
2023-03-06 18:33:00 -0600

    Add edit mode text to screen, test animation

--- Link.java
+++ Link.java
@@ -1,6 +1,5 @@
-import java.awt.image.BufferedImage;
-import java.io.File;
-import javax.imageio.ImageIO;
+import java.awt.Image;
+
 import java.awt.Graphics;
 
 public class Link {
@@ -14,15 +13,36 @@ public class Link {
     // Double for storing Link's movement speed
     double speed = 10;
 
-    // BufferedImage for storing Link image
-    BufferedImage image;
+    // Boolean to check whether Link is currently moving
+    boolean isMoving;
+
+    // Image arrays to store Link movement images
+    Image linkStill;
+    Image[] linkUp;
+    Image[] linkDown;
+    Image[] linkLeft;
+    Image[] linkRight;
+    static Image image = null;
 
     public Link() {
-        try {
-            this.image = ImageIO.read(new File(""));
-        } catch (Exception e) {
-            e.printStackTrace(System.err);
-            System.exit(1);
+        if (image == null) {
+            linkStill = View.loadImage("res/link/still/0.png");
+
+            linkUp = new Image[11];
+            for (int i = 0; i < linkUp.length; i++)
+                linkUp[i] = View.loadImage("res/link/up/" + i + ".png");
+
+            linkDown = new Image[13];
+            for (int i = 0; i < linkDown.length; i++)
+                linkDown[i] = View.loadImage("res/link/down/" + i + ".png");
+
+            linkLeft = new Image[13];
+            for (int i = 0; i < linkLeft.length; i++)
+                linkLeft[i] = View.loadImage("res/link/left/" + i + ".png");
+
+            linkRight = new Image[13];
+            for (int i = 0; i < linkRight.length; i++)
+                linkRight[i] = View.loadImage("res/link/right/" + i + ".png");
         }
     }
 
@@ -36,7 +56,13 @@ public class Link {
     }
 
     public void draw(Graphics g) {
+        if (!isMoving) {
+            g.drawImage(linkStill, x - View.scroll_x, y - View.scroll_y, null);
+            return;
+        }
 
+        g.drawImage(linkUp[3], x - View.scroll_x, y - View.scroll_y, null);
+        
     }
 
     // TODO: #2 Fix Link's position if colliding with tile
--- View.java
+++ View.java
@@ -8,6 +8,7 @@ import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import java.io.File;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Image;
 
 class View extends JPanel {
@@ -17,13 +18,13 @@ class View extends JPanel {
 	public static final int minWidth = 0, minHeight = 0, maxWidth = 700, maxHeight = 500;
 
 	// Integers to store window's scroll position
-	int scroll_x, scroll_y;
+	public static int scroll_x, scroll_y;
 
 	// BufferedImage variables for various tile images
 	BufferedImage cyanTile, greenTile, magentaTile, redTile;
 	
 	// Array of Link images
-	Image[] links;
+	// Image[] links;
 
 	public View(Controller c, Model m) {
 		this.model = m;
@@ -31,10 +32,10 @@ class View extends JPanel {
 		
 		// Load tile images into memory
 		try {
-			this.greenTile = ImageIO.read(new File("green_tile.jpg"));
-			this.redTile = ImageIO.read(new File("red_tile.jpg"));
-			this.magentaTile = ImageIO.read(new File("purple_tile.jpg"));
-			this.cyanTile = ImageIO.read(new File("cyan_tile.jpg"));
+			this.greenTile = ImageIO.read(new File("res/tiles/green.jpg"));
+			this.redTile = ImageIO.read(new File("res/tiles/red.jpg"));
+			this.magentaTile = ImageIO.read(new File("res/tiles/purple.jpg"));
+			this.cyanTile = ImageIO.read(new File("res/tiles/cyan.jpg"));
 		} catch (Exception e) {
 			e.printStackTrace(System.err);
 			System.exit(1);
@@ -44,9 +45,9 @@ class View extends JPanel {
 		model.unmarshal(Json.load("map.json"));
 
 		// Load link images into array; ignoring 0th index so indices align with file names
-		links = new Image[51];
-		for (int i = 1; i < links.length; i++)
-			links[i] = loadImage("link" + i + ".png");
+		// links = new Image[51];
+		// for (int i = 1; i < links.length; i++)
+		// 	links[i] = loadImage("link" + i + ".png");
 	}
 
 	// Method for loading image
@@ -60,8 +61,6 @@ class View extends JPanel {
 		}
 		return image;
 	}
-
-	// TODO: #3 Add text for edit mode on window instead of title
 
 	// Method for drawing screen for each frame
 	public void paintComponent(Graphics g) {
@@ -98,6 +97,14 @@ class View extends JPanel {
 			if (tile.x >= maxWidth && tile.y >= maxHeight) g.drawImage(cyanTile, x, y, null);
 		}
 
-		g.drawImage(links[1], model.link.x - scroll_x, model.link.y - scroll_y, null);
+		// Make Link draw himself onto screen
+		model.link.draw(g);
+
+		// If edit mode is currently on, display text to screen
+		if (Controller.editOn) {
+			g.setColor(new Color(255, 255, 255));
+			g.setFont(new Font("SansSerif", Font.BOLD, 24));
+			g.drawString("Edit mode is ON", View.minWidth + 25, View.minHeight + 35);
+		}
 	}
 }
